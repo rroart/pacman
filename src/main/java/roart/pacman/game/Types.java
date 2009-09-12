af99923fc6525b43a270c5eb636695a0b77fc848
Roar Thronæs <roart@nvg.ntnu.no>
2009-09-12 16:18:09 +0000

    Initial revision

---
+++ src/main/java/roart/pacman/game/Types.java
@@ -0,0 +1,5 @@
+package roart.pacman.game;
+
+//enum typ {classSpecialWall(1),classWall(2),classFood(3),classSuperFood(4),classBlank(5),classBonusPoint(6),classBonusLife(7),classPacman(8),classGhost(9),classOther(10);
+//};
+
