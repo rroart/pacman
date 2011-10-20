3c6ff5a86029714f02b48c435d119c817adcb6cc
Roar Thronæs <roart@nvg.ntnu.no>
2011-10-20 08:33:36 +0000

    Initial revision

---
+++ src/main/java/roart/pacman/game/Sizes.java
@@ -0,0 +1,32 @@
+package roart.pacman.game;
+
+public interface Sizes {
+
+int BOARDWIDTH = 33;
+int BOARDHEIGHT = 23;
+
+int UNITWIDTH = 16; 
+int UNITHEIGHT = 16;
+    //static int UNITWIDTH 1	//character 1x1 contains "pixmaps"
+    //static int UNITHEIGHT 1
+
+//defines different sizes for different fonts
+//static int TEXTWIDTH 8
+//static int TEXTHEIGHT 8
+//static int FONT1 "5x8"
+//static int FONT2 "5x7"
+//static int TEXTWIDTH 8
+//static int TEXTHEIGHT 16
+//static int FONT1 "8x16"
+//static int FONT2 "7x14"
+
+int TEXTCHARSINLINE = 15;	//text characters befors board begins
+int FOODS=168;	//how many food items there are on each board
+
+    //public    static int TEXTSPACE = TEXTWIDTH*TEXTCHARSINLINE		;
+    //public    static int WINWIDTH = BOARDWIDTH*UNITWIDTH+TEXTSPACE+20;
+    //public    static int WINHEIGHT = BOARDHEIGHT*UNITHEIGHT+40;
+
+
+
+}
