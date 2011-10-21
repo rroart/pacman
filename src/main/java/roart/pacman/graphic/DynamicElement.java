aebeb5f11c112b6d2092a69a2e0d938e6292367d
Roar Thronæs <roart@nvg.ntnu.no>
2011-10-21 11:32:11 +0000

    Initial revision

---
+++ src/main/java/roart/pacman/graphic/DynamicElement.java
@@ -0,0 +1,9 @@
+package roart.pacman.graphic;
+
+import java.awt.image.BufferedImage;
+
+public class DynamicElement extends GraphicElement {
+
+    public BufferedImage /*GID_TYPE*/ getgid() {return getPixmap();}
+
+}
